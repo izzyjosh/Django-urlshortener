ab058670d79ff4788443cf3d66fa6224a016df66
izzyjosh <calebayuba222@gmail.com>
2023-03-23 13:20:00 +0100

    made some changes to this fule

--- main/models.py
+++ main/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Url(models.Model):
-    url = models.URLField()
-    short_url = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.short_url
+	url = models.URLField()
+	short_url = models.CharField(max_length=30)
+    
+	def __str__(self):
+		return self.short_url
