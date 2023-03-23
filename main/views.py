f173cee8e448fd32ed5e02c92434ce44ad20ec78
izzyjosh <calebayuba222@gmail.com>
2023-03-23 13:20:41 +0100

    added a functuon to redirect the user to the main page

--- main/views.py
+++ main/views.py
@@ -5,26 +5,27 @@ from django.contrib import messages
 
 
 def shorturl(request):
-	def gen_short(url):
+	def gen_short():
 		choice = "abcdefghijklmnopqrstuvwxyz0123456789"
 		short = "".join(random.choices(choice, k=6))
 		return short
-		
 	
+	short_url = gen_short()
 	if request.method == "POST":
+		url = request.POST.get("url")
 		
-		url= request.POST.get("url")
-		short_url = gen_short(url)
 		if not Url.objects.filter(url=url).exists():
-			Url.objects.create(
-			      url=url, 
-			      short_url=short_url
-		       )
-			messages.info(request, f"{request.META['HTTP_HOST']}/{short_url}")
-		else:
-			u = Url.objects.get(url=url)  
-			messages.info(request, f"{request.META['HTTP_HOST']}/{u}")
-			
-	
+			Url.objects.create(url=url, short_url=short_url)
 		
+			messages.info(request, f"{request.META['HTTP_HOST']}/{short_url}")
+			
+		else:
+			u = Url.objects.get(url=url)
+			messages.info(request, f"{request.META['HTTP_HOST']}/{u.short_url}")
+			
 	return render(request, "url.html")
+	
+	
+def main_url(request, str):
+	u = Url.objects.get(short_url=str)
+	return redirect(u.url)
